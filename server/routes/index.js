496b03b5001860ed737f4b9d9c5bc612d85aa4ef
nithin <nthomas@Falabella.cl>
2020-09-27 12:07:29 +0530

    minor improvements

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var { sortProducts } = require("../utils/utils");
-const { Client } = require('@elastic/elasticsearch')
 var { response } = require("../data/sampleData");
+const { Client } = require('@elastic/elasticsearch')
 
-router.get('/getSimilarProducts', function (req, res, next) {
+const client = new Client({ node: 'http://localhost:9200' })
+
+async function getSku(){
+  client.search({
+    index: 'test2',
+    body: {
+      "query": {
+        "match": {
+          "product_skuId": "881886505"
+        }
+      }
+    }
+  }, (err, result) => {
+    if (err) console.log(err)
+    return result;
+  })
+}
+
+router.get('/getSimilarProducts', async function (req, res, next) {
   //TODO: validate request
 
   try {
@@ -12,23 +30,8 @@ router.get('/getSimilarProducts', function (req, res, next) {
     const { selectedProduct = {}, results = [] } = response
     // const sortedResults = sortProducts(selectedProduct, results)
 
-
-
-    const client = new Client({ node: 'http://localhost:9200' })
-
-    client.search({
-        index: 'test2',
-        body: {
-          "query": {
-            "match": {
-              "product_skuId": "881886505"
-            }
-          }
-        }
-      }, (err, result) => {
-        res.status(200).send({ data: result })
-        if (err) console.log(err)
-      })
+    const skuData = await getSku();
+    res.send(skuData);
     
   } catch (err) {
     res.status(500).send({ error: "Some error occured" })
