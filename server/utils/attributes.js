02141130055ee0377e76e42e2f43f694afb39c36
Piyush Das <pdas@falabella.cl>
2020-09-27 12:47:41 +0530

    [Piyush]  getting result set

---
+++ server/utils/attributes.js
@@ -0,0 +1,14 @@
+// TODO: Make configurable
+
+const attributes = [
+    "product.parentCategoryNames",
+    "variant.primayColor",
+    "variant.size",
+    "variant.colorGroup",
+    "product.brandName" ,
+    "product.tipo"
+]
+
+module.exports = {
+    attributes
+}
